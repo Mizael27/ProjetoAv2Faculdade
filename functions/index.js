6ec767c3d201f7fc75f160fdad1ba9ee8849e0d8
mizael27 <mizael27@live.com>
2019-06-07 08:20:31 -0300

    atual

--- functions/index.js
+++ functions/index.js
@@ -1,26 +1,31 @@
 const functions = require('firebase-functions');
+// Required for side-effects
 
-
-// Para acessar a database  \/
+// Para acessar a database ví na documentação do Firebase que necessita desse codigo \/
 const admin = require('firebase-admin');
 admin.initializeApp();
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
 
+// Função básica para inserir nova questão
 
-
-// Take the text parameter passed to this HTTP endpoint and insert it into the
-// Realtime Database under the path /messages/:pushId/original
-exports.addMessage = functions.https.onRequest(async (req, res) => {
-    // Grab the text parameter.
-    const original = req.query.text;
-    // Push the new message into the Realtime Database using the Firebase Admin SDK.
-    const snapshot = await admin.database().ref('/messages').push({original: original});
-    // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
-    res.redirect(303, snapshot.ref.toString());
-  });
-  
+  exports.salvarQuestao = functions.https.onCall((data, context) => {
+    // Obtemos os dados
+    const questao = data.questao;
+    const correta = data.correta;
+    const errada = data.errada;
+    var db = firebase.firestore();
+    db.collection("questoes").doc(size.toString()).set({
+      questao: questao,
+      respcorreta: correta,
+      resperrada: errada
+    })
+    .then(function() {
+      return true;
+    })
+    .catch(function(error) {
+      return false;
+    });
+  })
+  /*
+exports.teste = functions.https.onRequest((request, response) => {
+    response.send("Hello from Firebase!");
+});*/
